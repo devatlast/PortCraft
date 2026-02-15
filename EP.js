c3b70662501212f1f170105af0cdc5613dc47af8
olawale <eolawale33@gmail.com>
2026-02-15 14:32:24 +0100

    integrated glass-morphism for text-box and dynamic persuasion page logic

--- EP.js
+++ EP.js
@@ -1,3 +1,64 @@
+
+//the persuasion section
+let clickCount =0;
+const messages = [
+    "Your professional identity deserves a digital home that works as hard as you do.",
+    "Stop losing precious billable hours to manual paperworkand disorganized layouts",
+    "Secure your business revenue with professional invoices that guarantees payment.",
+    "Impress every potential client with a sleek, polished showcase of your skills.",
+    "Take control of your full career growth by unlocking these essential tools."
+];
+
+const clickmebtn = document.querySelector('#click-me');
+const displayText =document.querySelector('#main-text');
+
+clickmebtn.addEventListener( 'click', () => {
+    clickCount++;
+    if ( clickCount < 5) {
+        displayText.textContent = messages[clickCount];
+    } else {
+        displayText.textContent = "Are you ready to transform your workflow?";
+        document.querySelector('#click-me').classList.add('hidden');
+        document.querySelector('#choices').classList.remove('hidden');
+        document.querySelector('#mode-display').classList.add('hidden');
+    }
+});
+
+document.querySelector('#yes-btn').addEventListener('click', () => {
+    document.querySelector('#persuasion-section').classList.add('hidden');
+    document.querySelector('#workshop').classList.remove('hidden');
+});
+
+let noCount=0;
+const desperateMessages = [
+    "Are you sure? Your future self might regret this!",
+    "But... The professional world is waiting for you!",
+    "Think of all the invoices you could be sending and how sleek your portfolio looks.",
+    "Please click, Yes?",
+    "Now you're being difficult.",
+];
+
+const noBtn = document.querySelector('#no-btn');
+const yesBtn = document.querySelector('#yes-btn');
+
+// logic for choices buttons and to make the yes button grow bigger
+noBtn.addEventListener('click', () => {
+    if (noCount < desperateMessages.length) {
+        document.querySelector('#main-text').textContent = desperateMessages[noCount];
+        noCount++;
+    } else {
+        document.querySelector('#main-text').textContent = "Fine i'll make Yes impossible to miss!";
+    }
+
+    let nowScale = 1 + (noCount * 0.5);
+    yesBtn.style.transform = `scale(${nowScale})`;
+    yesBtn.style.transition = "transform 0.3s ease";
+})
+
+
+
+
+// logic to rps game
 let playerscore = 0;
 let computerscore = 0;
 
@@ -60,6 +121,9 @@ buttons.forEach(btn =>{
     const result = matchRound(playerChoice, computerChoice);
       });
 });
+// End of rps logic
+
+
 
 
 //event listener for dark mode button
